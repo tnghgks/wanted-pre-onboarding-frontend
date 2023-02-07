92384283dda6ed588752529eef5e1c69df79e758
tnghgks <tnghgks@naver.com>
2023-02-08 01:50:28 +0900

    feat: SignIn 페이지에서 LoginForm 컴포넌트 분리 및 유효성 검사 추가

---
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -0,0 +1,45 @@
+import { useCallback, useMemo } from "react";
+import useInput from "../../../hook/common/useInput";
+import Button from "../../common/Button/Button";
+import { validator } from "../../../util/validator";
+import { S } from "./style";
+import Input from "../../common/Input/Input";
+
+export default function LoginForm() {
+  const [email, isValidEmail] = useInput("", validator.email);
+  const [password, isValidPw] = useInput("", validator.password);
+  const isDisabled = useMemo(() => ![isValidEmail.value, isValidPw.value].every((valid) => valid), [isValidEmail, isValidPw]);
+
+  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }, []);
+
+  return (
+    <S.FormContainer onSubmit={handleSubmit}>
+      <Input
+        label="이메일"
+        id="email"
+        type="text"
+        placeholder="사용하실 이메일을 입력해주세요."
+        data-testid="email-input"
+        required
+        {...email}
+        error={isValidEmail.message}
+      />
+      <Input
+        label="패스워드"
+        id="password"
+        type="password"
+        placeholder="8자리 이상 패스워드를 입력해주세요."
+        data-testid="password-input"
+        required
+        {...password}
+        error={isValidPw.message}
+      />
+      <Button bgColor="--accent-color" txtColor="white" data-testid="signin-button" disabled={isDisabled}>
+        로그인
+      </Button>
+      <S.RegisterLink to="/signup">회원가입 하러가기 ➡</S.RegisterLink>
+    </S.FormContainer>
+  );
+}
--- src/pages/SignIn/style.tsx
+++ src/pages/SignIn/style.tsx
@@ -6,7 +6,6 @@ const Container = styled.article`
   left: 50%;
   transform: translate(-50%, -50%);
   width: 400px;
-  height: 300px;
   background-color: var(--color-type-01);
 `;
 
@@ -22,30 +21,7 @@ const Title = styled.h2`
   font-size: 2rem;
 `;
 
-const FormContainer = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 40px;
-
-  input {
-    width: 330px;
-    height: 40px;
-    padding-left: 10px;
-    margin-bottom: 25px;
-    border-radius: 5px;
-    border: none;
-    background-color: var(--color-type-04);
-    font-size: 2rem;
-  }
-
-  button {
-    margin-bottom: 10px;
-  }
-`;
-
 export const S = {
   Container,
   Title,
-  FormContainer,
 };
