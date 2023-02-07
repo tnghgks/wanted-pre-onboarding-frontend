4f04e38c15525269e3f84373a045cfc8cfe52af5
tnghgks <tnghgks@naver.com>
2023-02-08 01:50:49 +0900

    feat: SignUp 페이지에서 RegisterForm 컴포넌트 분리 및 유효성 검사 추가

---
+++ src/components/auth/RegisterForm/style.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 20px;
+
+  button {
+    margin-bottom: 10px;
+  }
+`;
+
+const RegisterLink = styled(Link)`
+  color: var(--bg-color);
+`;
+
+export const S = {
+  FormContainer,
+  RegisterLink,
+};
