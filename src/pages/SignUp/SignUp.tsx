ef2f0b6fab677a398750590107fa6403f8b72e6a
HOSU HAN <tnghgks@naver.com>
2023-02-07 20:40:59 +0900

    design: SignUp 컴포넌트 마크업 및 UI 생성 (#6)
    * feat: style 컴포넌트 생성
    
    * feat: SignUp 컴포넌트 마크업 및 UI 생성

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,3 +1,19 @@
+import Button from "../../components/common/Button/Button";
+import { S } from "./style";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <main>
+      <h1 className="ir-hidden">회원가입 페이지</h1>
+      <S.Container>
+        <S.Title>회원가입</S.Title>
+        <S.FormContainer>
+          <input type="email" placeholder="이메일" />
+          <input type="password" placeholder="패스워드" />
+          <input type="password" placeholder="패스워드 확인" />
+          <Button theme="red">회원가입</Button>
+        </S.FormContainer>
+      </S.Container>
+    </main>
+  );
 }
