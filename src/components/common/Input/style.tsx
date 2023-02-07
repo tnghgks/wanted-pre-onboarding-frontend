cf8e64b165e2aa09e0ea31dc26769d9a17eb7579
tnghgks <tnghgks@naver.com>
2023-02-08 01:43:00 +0900

    feat: Input 컴포넌트로 분리

---
+++ src/components/common/Input/style.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
+const Label = styled.label`
+  align-self: flex-start;
+  margin-left: 40px;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 330px;
+  height: 40px;
+  padding-left: 10px;
+  border-radius: 5px;
+  border: none;
+  background-color: var(--color-type-04);
+  font-size: 1.6rem;
+`;
+
+const InputErrorMessage = styled(ErrorMessage)`
+  margin-top: 5px;
+  margin-bottom: 20px;
+`;
+
+export const S = {
+  Label,
+  Input,
+  InputErrorMessage,
+};
