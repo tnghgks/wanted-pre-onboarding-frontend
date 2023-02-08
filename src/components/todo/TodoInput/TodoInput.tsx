9eb50dec61564cc433dade191edc8d082940c5ab
HOSU HAN <tnghgks@naver.com>
2023-02-08 21:13:04 +0900

    feat: Todo 생성 기능 추가 (#16)
    * feat: axiosAuthInstance request시 LocalStorage 토큰 값 추가
    
    * feat: createTodo api 추가
    
    * feat: 투두 생성 기능 추가

--- src/components/todo/TodoInput/TodoInput.tsx
+++ src/components/todo/TodoInput/TodoInput.tsx
@@ -1,13 +1,31 @@
+import { useCallback } from "react";
 import { S } from "./style";
 import iconEnter from "../../../assets/icon/icon-enter.svg";
+import todoApi from "../../../services/api/todo";
+import useInput from "../../../hooks/common/useInput";
 
 export default function TodoInput() {
+  const [todo] = useInput("");
+
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const response = await todoApi.createTodo(todo.value);
+
+      if (response?.status) {
+        alert("투두를 입력해주세요.");
+      }
+    },
+    [todo.value]
+  );
+
   return (
-    <S.Container>
+    <S.Container onSubmit={handleSubmit}>
       <S.Button data-testid="new-todo-add-button">
         <img src={iconEnter} alt="입력 버튼" />
       </S.Button>
-      <S.Input type="text" data-testid="new-todo-input" />
+      <S.Input type="text" data-testid="new-todo-input" {...todo} />
     </S.Container>
   );
 }
