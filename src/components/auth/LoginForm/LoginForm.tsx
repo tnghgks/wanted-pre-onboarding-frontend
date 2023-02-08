69a5529b75eee70a60d8655055aa4bb6589e47c6
tnghgks <tnghgks@naver.com>
2023-02-08 11:49:37 +0900

    feat: LoginForm 컴포넌트 로그인 기능 추가

--- src/components/auth/LoginForm/LoginForm.tsx
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -1,18 +1,36 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import useInput from "../../../hooks/common/useInput";
 import Button from "../../common/Button/Button";
 import { validator } from "../../../util/validator";
 import { S } from "./style";
 import Input from "../../common/Input/Input";
+import authApi from "../../../services/api/auth";
+import { useNavigate } from "react-router-dom";
+import ErrorMessage from "../../common/ErrorMessage/ErrorMessage";
 
 export default function LoginForm() {
+  const navigate = useNavigate();
   const [email, isValidEmail] = useInput("", validator.email);
   const [password, isValidPw] = useInput("", validator.password);
+  const [error, setError] = useState("");
   const isDisabled = useMemo(() => ![isValidEmail.value, isValidPw.value].every((valid) => valid), [isValidEmail.value, isValidPw.value]);
 
-  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  }, []);
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const res = await authApi.signIn({ email: email.value, password: password.value });
+
+      if (res?.status === 401) {
+        return setError("이메일 또는 비밀번호가 틀렸습니다.");
+      }
+      if (res?.status === 200) {
+        localStorage.setItem("token", res.data.access_token);
+        return navigate("/todo");
+      }
+    },
+    [email.value, password.value, navigate]
+  );
 
   return (
     <S.FormContainer onSubmit={handleSubmit}>
@@ -36,6 +54,7 @@ export default function LoginForm() {
         {...password}
         error={isValidPw.message}
       />
+      <ErrorMessage>{error}</ErrorMessage>
       <Button bgColor="--accent-color" txtColor="white" data-testid="signin-button" disabled={isDisabled}>
         로그인
       </Button>
