105ddea21d24e418a45e767c5d14c5c10f9c99c6
tnghgks <tnghgks@naver.com>
2023-02-08 19:36:30 +0900

    feat: 투두 페이지 컴포넌트 UI 생성

---
+++ src/pages/Todo/style.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+  width: 100%;
+  max-width: 600px;
+  min-height: 700px;
+  background-color: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--accent-color);
+  padding-right: 20px;
+  align-items: center;
+`;
+const Title = styled.h2`
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  color: white;
+  font-family: "LINESeedKR-Bd";
+  font-size: 2rem;
+`;
+
+const TodoContainer = styled.section`
+  width: 100%;
+  height: 100%;
+  padding: 50px;
+`;
+
+export const S = {
+  Container,
+  Header,
+  Title,
+  TodoContainer,
+};
