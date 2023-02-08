fb9d5fc9e493c542d3b8c39a7f1d21e64cc76762
tnghgks <tnghgks@naver.com>
2023-02-08 11:50:18 +0900

    feat: ProtectedRoute 컴포넌트 localStorage에서 token 체크 추가

--- src/components/HOC/auth/ProtectedRoute.tsx
+++ src/components/HOC/auth/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const token = false;
+  const token = localStorage.getItem("token");
 
   if (!token) {
     return <Navigate to="/signin" />;
