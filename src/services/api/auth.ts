784bf2397cff008381a7f77c1dabd2c0e6799e46
tnghgks <tnghgks@naver.com>
2023-02-08 11:48:08 +0900

    feat: signIn 함수 추가

--- src/services/api/auth.ts
+++ src/services/api/auth.ts
@@ -14,8 +14,22 @@ const signUp = async (body: { email: string; password: string }) => {
   }
 };
 
+const signIn = async (body: { email: string; password: string }) => {
+  try {
+    const response = await axiosInstance.post("/auth/signin", body);
+
+    return response;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error.response;
+    }
+    console.log(error);
+  }
+};
+
 const authApi = {
   signUp,
+  signIn,
 };
 
 export default authApi;
