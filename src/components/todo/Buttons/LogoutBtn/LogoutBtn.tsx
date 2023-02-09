9eceb37f47b5d99c684c675b07386448d7665d10
tnghgks <tnghgks@naver.com>
2023-02-09 18:33:38 +0900

    feat: 로그아웃 기능 추가

--- src/components/todo/Buttons/LogoutBtn/LogoutBtn.tsx
+++ src/components/todo/Buttons/LogoutBtn/LogoutBtn.tsx
@@ -1,9 +1,15 @@
+import { useCallback } from "react";
 import logoutIcon from "../../../../assets/icon/icon-logout.svg";
 import { S } from "./style";
 
-export default function LogoutBtn({ onClick }: { onClick?: () => void }) {
+export default function LogoutBtn() {
+  const handleLogout = useCallback(() => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  }, []);
+
   return (
-    <S.Button onClick={onClick}>
+    <S.Button onClick={handleLogout} type="button">
       <img src={logoutIcon} alt="로그아웃 버튼" />
     </S.Button>
   );
