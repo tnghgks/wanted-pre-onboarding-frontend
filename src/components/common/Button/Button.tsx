6ed9ffea4dc7fb105675a540b107bea2ef91dbde
tnghgks <tnghgks@naver.com>
2023-02-10 02:59:16 +0900

    fix: Button 컴포넌트 명시한 Props 이외 속성들 rest 문법으로 button 요소에 적용

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -7,9 +7,9 @@ interface Props {
   disabled?: boolean;
 }
 
-export default function Button({ children, bgColor, txtColor, disabled = false }: Props) {
+export default function Button({ children, bgColor, txtColor, disabled = false, ...rest }: Props) {
   return (
-    <S.Button bgColor={bgColor} txtColor={txtColor} disabled={disabled}>
+    <S.Button bgColor={bgColor} txtColor={txtColor} disabled={disabled} {...rest}>
       {children}
     </S.Button>
   );
