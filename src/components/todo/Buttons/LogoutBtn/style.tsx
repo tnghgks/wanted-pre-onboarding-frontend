b56fe8134b3ea00bbdecba829e946e17433b34c6
tnghgks <tnghgks@naver.com>
2023-02-09 18:16:08 +0900

    feat: 로그아웃 버튼 폴더 위치 이동

---
+++ src/components/todo/Buttons/LogoutBtn/style.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 24px;
+  height: 24px;
+  padding: 0;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+export const S = { Button };
