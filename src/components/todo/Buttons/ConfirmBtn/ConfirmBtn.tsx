b1f1c5146931a00778449b174fa88dd958821fba
tnghgks <tnghgks@naver.com>
2023-02-17 23:28:50 +0900

    fix: 불필요한 코드 삭제

--- src/components/todo/Buttons/ConfirmBtn/ConfirmBtn.tsx
+++ src/components/todo/Buttons/ConfirmBtn/ConfirmBtn.tsx
@@ -1,9 +1,9 @@
 import confirmIcon from "../../../../assets/icon/icon-confirm.svg";
 import { S } from "./style";
 
-export default function ConfirmBtn({ onClick }: { onClick?: () => void }) {
+export default function ConfirmBtn() {
   return (
-    <S.Button onClick={onClick} type="submit" data-testid="submit-button">
+    <S.Button type="submit" data-testid="submit-button">
       <img src={confirmIcon} alt="제출 버튼" />
     </S.Button>
   );
